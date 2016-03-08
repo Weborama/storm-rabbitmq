6841a9ffc7d7cc077c3c7a7ae87b36489ccb3e44
Costin Apostol <capostol@weborama.com>
2016-03-08 11:45:41 +0100

    fix throws TimeoutException

--- src/main/java/io/latent/storm/rabbitmq/RabbitMQProducer.java
+++ src/main/java/io/latent/storm/rabbitmq/RabbitMQProducer.java
@@ -1,7 +1,14 @@
 package io.latent.storm.rabbitmq;
 
 import backtype.storm.topology.ReportedFailedException;
-import com.rabbitmq.client.*;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.AlreadyClosedException;
+import com.rabbitmq.client.BlockedListener;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ShutdownListener;
+import com.rabbitmq.client.ShutdownSignalException;
 import io.latent.storm.rabbitmq.config.ConnectionConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 public class RabbitMQProducer implements Serializable {
   private final Declarator declarator;
@@ -21,8 +29,7 @@ public class RabbitMQProducer implements Serializable {
 
   private boolean blocked = false;
 
-  public RabbitMQProducer()
-  {
+  public RabbitMQProducer() {
     this(new Declarator.NoOp());
   }
 
@@ -31,14 +38,18 @@ public class RabbitMQProducer implements Serializable {
   }
 
   public void send(Message message) {
-    if (message == Message.NONE) return;
+    if (message == Message.NONE)
+      return;
     sendMessageWhenNotBlocked((Message.MessageForSending) message);
   }
 
   private void sendMessageWhenNotBlocked(Message.MessageForSending message) {
     while (true) {
       if (blocked) {
-        try { Thread.sleep(100); } catch (InterruptedException ie) { }
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException ie) {
+        }
       } else {
         sendMessageActual(message);
         return;
@@ -49,15 +60,17 @@ public class RabbitMQProducer implements Serializable {
   private void sendMessageActual(Message.MessageForSending message) {
 
     reinitIfNecessary();
-    if (channel == null) throw new ReportedFailedException("No connection to RabbitMQ");
+    if (channel == null)
+      throw new ReportedFailedException("No connection to RabbitMQ");
     try {
       AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
-                                                                .contentType(message.getContentType())
-                                                                .contentEncoding(message.getContentEncoding())
-                                                                .deliveryMode((message.isPersistent()) ? 2 : 1)
-                                                                .headers(message.getHeaders())
-                                                                .build();
-      channel.basicPublish(message.getExchangeName(), message.getRoutingKey(), properties, message.getBody());
+          .contentType(message.getContentType())
+          .contentEncoding(message.getContentEncoding())
+          .deliveryMode((message.isPersistent()) ? 2 : 1)
+          .headers(message.getHeaders())
+          .build();
+      channel.basicPublish(message.getExchangeName(), message.getRoutingKey(), properties,
+          message.getBody());
     } catch (AlreadyClosedException ace) {
       logger.error("already closed exception while attempting to send message", ace);
       reset();
@@ -67,8 +80,12 @@ public class RabbitMQProducer implements Serializable {
       reset();
       throw new ReportedFailedException(ioe);
     } catch (Exception e) {
-      logger.warn("Unexpected error while sending message. Backing off for a bit before trying again (to allow time for recovery)", e);
-      try { Thread.sleep(1000); } catch (InterruptedException ie) { }
+      logger.warn("Unexpected error while sending message. "
+          + "Backing off for a bit before trying again (to allow time for recovery)", e);
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException ie) {
+      }
     }
   }
 
@@ -120,10 +137,13 @@ public class RabbitMQProducer implements Serializable {
     }
   }
 
-  private Connection createConnection() throws IOException {
+  private Connection createConnection() throws IOException, TimeoutException {
     ConnectionFactory connectionFactory = connectionConfig.asConnectionFactory();
-    Connection connection = connectionConfig.getHighAvailabilityHosts().isEmpty() ? connectionFactory.newConnection()
-        : connectionFactory.newConnection(connectionConfig.getHighAvailabilityHosts().toAddresses());
+    Connection connection =
+        connectionConfig.getHighAvailabilityHosts().isEmpty()
+            ? connectionFactory.newConnection()
+            : connectionFactory.newConnection(
+                connectionConfig.getHighAvailabilityHosts().toAddresses());
     connection.addShutdownListener(new ShutdownListener() {
       @Override
       public void shutdownCompleted(ShutdownSignalException cause) {
@@ -131,18 +151,15 @@ public class RabbitMQProducer implements Serializable {
         reset();
       }
     });
-    connection.addBlockedListener(new BlockedListener()
-    {
+    connection.addBlockedListener(new BlockedListener() {
       @Override
-      public void handleBlocked(String reason) throws IOException
-      {
+      public void handleBlocked(String reason) throws IOException {
         blocked = true;
         logger.warn(String.format("Got blocked by rabbitmq with reason = %s", reason));
       }
 
       @Override
-      public void handleUnblocked() throws IOException
-      {
+      public void handleUnblocked() throws IOException {
         blocked = false;
         logger.warn(String.format("Got unblocked by rabbitmq"));
       }
